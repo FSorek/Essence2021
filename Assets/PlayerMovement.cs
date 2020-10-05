879f00113f68b8a9ade1564a7af333b15ce04034
fsorek <filip.sorek96@gmail.com>
2020-10-05 23:58:27 +0200

    Added basic player movement

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float panSpeed = 1f;
+
+    private void Update()
+    {
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        var movement = panSpeed * Time.deltaTime * new Vector3(horizontal, 0, 0);
+        
+        transform.Translate(movement);
+    }
+}
