a401a8670aa9a7eabc55eaab60ec36e2cdbf31c3
fsorek <filip.sorek96@gmail.com>
2020-10-06 13:59:41 +0200

    Added simple world generation, showing position on UI for first segment

---
+++ Assets/WorldGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class WorldGenerator : MonoBehaviour
+{
+    public int TotalSegments => segments.Count;
+    public WorldSegment CurrentViewingSegment { get; private set; }
+    
+    [SerializeField] private int startingSegmentCount;
+    [SerializeField] private WorldSegment segmentPrefab;
+    private List<WorldSegment> segments;
+
+    private void Awake()
+    {
+        var preSpawnedSegments = FindObjectsOfType<WorldSegment>();
+        segments = new List<WorldSegment>(preSpawnedSegments);
+        CurrentViewingSegment = segments.First();
+    }
+
+    private void Start()
+    {
+        if (segments.Count < startingSegmentCount)
+        {
+            int segmentsToAdd = startingSegmentCount - segments.Count;
+            for (int i = 0; i < segmentsToAdd; i++)
+                CreateSegment();
+        }
+    }
+
+    private void CreateSegment()
+    {
+        var newSegment = Instantiate(segmentPrefab);
+        newSegment.Anchor(segments.Last());
+        segments.Add(newSegment);
+    }
+}
---
+++ Assets/WorldSegment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldSegment : MonoBehaviour
+{
+    public float Length { get; private set; }
+
+    private void Awake()
+    {
+        Length = GetComponentInChildren<MeshRenderer>().bounds.size.x;
+    }
+
+    public void Anchor(WorldSegment anchoringSegment)
+    {
+        var anchorX = anchoringSegment.transform.position.x + Length;
+        transform.position = new Vector3(anchorX, 0, 0);
+    }
+}
