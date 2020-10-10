9a6e6e45bd3ffaa3abc33e84935574b42592297d
fsorek <filip.sorek96@gmail.com>
2020-10-10 03:36:10 +0200

    Added segment repeating when player moves around

--- Assets/Scripts/Game/WorldGenerator.cs
+++ Assets/Scripts/Game/WorldGenerator.cs
@@ -1,28 +1,56 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using UnityEngine;
 
 public class WorldGenerator : MonoBehaviour
 {
-    public WorldSegment CurrentViewingSegment => segments[currentSegmentIndex];
+    public WorldSegment CurrentViewingSegment => segments[playerSegmentIndex];
     
-    [SerializeField] private int startingSegmentCount;
+    [SerializeField][Min(3)] private int startingSegmentCount = 3;
     [SerializeField] private WorldSegment segmentPrefab;
-    private List<WorldSegment> segments;
-    private int currentSegmentIndex;
+    private ObservableCollection<WorldSegment> segments;
+    private int playerSegmentIndex;
+    private Transform player;
+    private float percentagePositionOnSegment;
 
     private void Awake()
     {
+        player = FindObjectOfType<PlayerMovement>().transform;
         var preSpawnedSegments = FindObjectsOfType<WorldSegment>();
-        segments = new List<WorldSegment>(preSpawnedSegments);
-        currentSegmentIndex = 0;
+        segments = new ObservableCollection<WorldSegment>(preSpawnedSegments);
     }
 
     private void Start()
     {
-        AlignPrepawnedSegments();
+        AlignWorldSpawnedSegments();
         SpawnSegments();
+        playerSegmentIndex = 0;
+    }
+
+    private void Update()
+    {
+        if (playerSegmentIndex == segments.Count - 1)
+            MoveFirstSegmentToLast();
+        else if(playerSegmentIndex == 0)
+            MoveLastSegmentToFirst();
+        
+        percentagePositionOnSegment = 1 - player.InverseTransformPoint(CurrentViewingSegment.transform.position).x / CurrentViewingSegment.Length;
+        if (percentagePositionOnSegment > 1.1f)
+            playerSegmentIndex++;
+        else if (percentagePositionOnSegment < -.1f)
+            playerSegmentIndex--;
+
+    }
+
+    private void AlignWorldSpawnedSegments()
+    {
+        for (int i = 0; i < segments.Count; i++)
+        {
+            var segment = segments[i];
+            if(i + 1 < segments.Count)
+                segments[i+1].AnchorLeft(segment);
+        }
     }
 
     private void SpawnSegments()
@@ -35,16 +63,6 @@ public class WorldGenerator : MonoBehaviour
             CreateSegment();
     }
 
-    private void AlignPrepawnedSegments()
-    {
-        for (int i = 0; i < segments.Count; i++)
-        {
-            var segment = segments[i];
-            if(i + 1 < segments.Count)
-                segments[i+1].AnchorLeft(segment);
-        }
-    }
-
     private void CreateSegment()
     {
         var newSegment = Instantiate(segmentPrefab);
@@ -55,17 +73,21 @@ public class WorldGenerator : MonoBehaviour
         segments.Add(newSegment);
     }
 
-    public void MoveFirstSegmentToLast()
+    private void MoveFirstSegmentToLast()
     {
         var firstSegment = segments.First();
         var lastSegment = segments.Last();
         firstSegment.AnchorRight(lastSegment);
+        segments.Move(0, segments.Count - 1);
+        playerSegmentIndex--;
     }
 
-    public void MoveLastSegmentToFirst()
+    private void MoveLastSegmentToFirst()
     {
         var firstSegment = segments.First();
         var lastSegment = segments.Last();
         lastSegment.AnchorLeft(firstSegment);
+        segments.Move(segments.Count - 1, 0);
+        playerSegmentIndex++;
     }
 }
