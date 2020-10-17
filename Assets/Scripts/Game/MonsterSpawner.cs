451d14d769d80ce9fa50e2d6136ff70c39e02499
fsorek <filip.sorek96@gmail.com>
2020-10-17 15:33:06 +0200

    Moving along the map adjusts location when moving to new segment

---
+++ Assets/Scripts/Game/MonsterSpawner.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MonsterSpawner : MonoBehaviour
+{
+    [SerializeField] private Transform monsterPrefab;
+    [SerializeField][Min(.5f)] private float spawningFrequency;
+    [SerializeField] private Collider spawnArea;
+
+    private void Start()
+    {
+        InvokeRepeating(nameof(Spawn), spawningFrequency, spawningFrequency);
+    }
+
+    private void Spawn()
+    {
+        var spawnPointX = transform.position.x - Random.Range(0, spawnArea.bounds.size.x);
+        var spawnPointZ = transform.position.z - spawnArea.bounds.size.z/2 + Random.Range(0, spawnArea.bounds.size.z);
+        var spawnPosition = new Vector3(spawnPointX, transform.position.y, spawnPointZ);
+
+        Instantiate(monsterPrefab, spawnPosition, Quaternion.identity, spawnArea.transform.parent);
+    }
+}
---
+++ Assets/Scripts/Game/SegmentMovementAdjustment.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(WorldPosition))]
+public class SegmentMovementAdjustment : MonoBehaviour
+{
+    private WorldPosition position;
+
+    private void Awake()
+    {
+        position = GetComponent<WorldPosition>();
+        position.OnSegmentChanged += AdjustPosition;
+    }
+
+    private void AdjustPosition(WorldSegment segment)
+    {
+        if (position.SegmentPosition > segment.Length)
+        {
+            var adjustedPosition = new Vector3(segment.transform.position.x, transform.position.y, transform.position.z);
+            transform.position = adjustedPosition;
+        }
+    }
+}
