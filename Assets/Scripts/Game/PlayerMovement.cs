b2f7c61c49e4d58b9f0af3c1e2ea37d69545c1e0
fsorek <filip.sorek96@gmail.com>
2020-10-18 17:39:50 +0200

    Added new input system, adjusted player movement

--- Assets/Scripts/Game/PlayerMovement.cs
+++ Assets/Scripts/Game/PlayerMovement.cs
@@ -2,16 +2,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float panSpeed = 1f;
+    private Vector3 movement;
 
-    private void Update()
+    public void Move(InputAction.CallbackContext context)
     {
-        var horizontal = Input.GetAxisRaw("Horizontal");
-        var movement = panSpeed * Time.deltaTime * new Vector3(horizontal, 0, 0);
-        
-        transform.Translate(movement);
+        var movementValue = context.ReadValue<float>() * panSpeed;
+        movement = new Vector3(movementValue, 0 ,0);
+    }
+
+    private void LateUpdate()
+    {
+        transform.Translate(movement * Time.deltaTime);
     }
 }
+
