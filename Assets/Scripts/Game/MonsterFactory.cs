1ed9d0984a50ee5e0ca309551bb3d822711afd56
fsorek <filip.sorek96@gmail.com>
2020-10-31 16:36:33 +0100

    Separated classes to own files

---
+++ Assets/Scripts/Game/MonsterFactory.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterFactory : IEntityFactory<Monster>
+{
+    private readonly List<Monster> monstersAlive = new List<Monster>();
+    
+    public IEnumerable<Monster> GetAliveEntities()
+    {
+        return monstersAlive;
+    }
+
+    public void CreateMonster(Monster monsterPrefab, Vector3 spawnPosition, Transform transformParent)
+    {
+        var monster = Object.Instantiate(monsterPrefab, spawnPosition, Quaternion.identity, transformParent);
+        monstersAlive.Add(monster);
+        monster.OnDeath += MonsterOnDeath;
+    }
+
+    private void MonsterOnDeath(Monster monster)
+    {
+        if(!monstersAlive.Contains(monster))
+            return;
+        monstersAlive.Remove(monster);
+        monster.OnDeath -= MonsterOnDeath;
+    }
+}
--- Assets/Scripts/Game/TargetFinder.cs
+++ Assets/Scripts/Game/TargetFinder.cs
@@ -36,30 +36,6 @@ public class TargetFinder<T> where T : class, IEntity
     }
 }
 
-public class MonsterFactory : IEntityFactory<Monster>
-{
-    private readonly List<Monster> monstersAlive = new List<Monster>();
-    
-    public IEnumerable<Monster> GetAliveEntities()
-    {
-        return monstersAlive;
-    }
-
-    public void CreateMonster(Monster monsterPrefab, Vector3 spawnPosition, Transform transformParent)
-    {
-        var monster = Object.Instantiate(monsterPrefab, spawnPosition, Quaternion.identity, transformParent);
-        monstersAlive.Add(monster);
-        monster.OnDeath += MonsterOnDeath;
-    }
-
-    private void MonsterOnDeath(Monster monster)
-    {
-        if(!monstersAlive.Contains(monster))
-            return;
-        monstersAlive.Remove(monster);
-        monster.OnDeath -= MonsterOnDeath;
-    }
-}
 public interface IEntityFactory<T> where T : class, IEntity
 {
     IEnumerable<T> GetAliveEntities();
