351f94eaefa4d1f7e32fc0a151fb065bcf9c9709
fsorek <filip.sorek96@gmail.com>
2020-11-16 20:20:23 +0100

    Removed highlighting obelisks

--- Assets/Scripts/Game/Obelisk.cs
+++ Assets/Scripts/Game/Obelisk.cs
@@ -5,18 +5,6 @@ public class Obelisk : MonoBehaviour, IEssenceHolder
     public Transform EssenceHolder => essenceHolder;
     public Essence CurrentEssence { get; private set; }
     [SerializeField] private Transform essenceHolder;
-    [SerializeField] private MeshRenderer mRenderer;
-    [SerializeField] private Material normalMaterial;
-    [SerializeField] private Material highlightMaterial;
-    public void Highlight()
-    {
-        mRenderer.material = highlightMaterial;
-    }
-
-    public void Dehighlight()
-    {
-        mRenderer.material = normalMaterial;
-    }
 
     public void AddEssence(Essence essence)
     {
